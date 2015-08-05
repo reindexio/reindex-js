d62db22c3fca9800632a7a823cc527fbcad3e7dd
Ville Immonen <ville.immonen@iki.fi>
2015-08-05 13:10:40 +0300

    Set the popup size for each provider

--- src/index.js
+++ src/index.js
@@ -3,11 +3,25 @@ import Promise from 'bluebird';
 import WinChan from 'winchan';
 
 const SUPPORTED_PROVIDERS = {
-  facebook: true,
-  github: true,
-  google: true,
-  twitter: true,
+  facebook: {
+    windowFeatures: { width: 500, height: 467 },
+  },
+  github: {
+    windowFeatures: { width: 1000, height: 600 },
+  },
+  google: {
+    windowFeatures: { width: 500, height: 467 },
+  },
+  twitter: {
+    windowFeatures: { width: 450, height: 635 },
+  },
 };
+
+function stringifyWindowFeatures(windowFeatures) {
+  return Object.keys(windowFeatures)
+    .map((key) => `${key}=${windowFeatures[key]}`)
+    .join(',');
+}
 
 class Reindex {
   /**
@@ -18,18 +32,20 @@ class Reindex {
   }
 
   /**
-   * Login via given `provider`.
+   * Login via given `providerName`.
    */
-  login(provider) {
+  login(providerName) {
+    const provider = SUPPORTED_PROVIDERS[providerName];
     invariant(
-      SUPPORTED_PROVIDERS[provider],
-      'Reindex.login(...): unknown provider "%s"'
+      provider,
+      'Reindex.login(...): unknown provider "%s"',
+      providerName
     );
     return new Promise((resolve, reject) => {
       WinChan.open({
-        url: `${this._url}/auth/${provider}`,
+        url: `${this._url}/auth/${providerName}`,
         relay_url: `${this._url}/auth/channel`,
-        window_features: 'width=950,height=850',
+        window_features: stringifyWindowFeatures(provider.windowFeatures),
       }, (error, response) => {
         if (error) {
           reject(error);
